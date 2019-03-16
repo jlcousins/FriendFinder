3b6f4755618e3dd8dc0460ec76c0683af8607897
Janavia Cousins <jlcousins18@gmail.com>
2019-03-16 02:28:36 -0400

    initial push

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+
+var friends = [
+    {
+        "name": "Nancy",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU7x9zll2aKo0D8gYj1lZ8AmgzMuy_ApL2H2JlqeUgGMJSq60e",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Phil",
+        "photo": "https://pro2-bar-s3-cdn-cf1.myportfolio.com/ca62d419d5386498727a1bd2a36de87d/22dfa56ea4db43630ee60490_rw_1920.jpg?h=c7de9de6a54d9e007424fa6857166b94",
+        "scores": [3, 4, 1, 4, 3, 2, 1, 3, 2, 2]
+    },
+    {
+        "name": "Drew",
+        "photo": "https://photos.headshotcrew.com/styles/large/s3/photos/01/47/21/74825-alan-moore-final.jpg?itok=VSo69ejt",
+        "scores": [4, 2, 1, 3, 2, 2, 3, 3, 2, 1]
+    },
+    {
+        "name": "Jasmine",
+        "photo": "https://pro2-bar-s3-cdn-cf1.myportfolio.com/5660ff2c512a9ee614b8b88c9e8ffc95/2cb92ee50c74fb6ff98b0aa4_rw_1920.jpg?h=81b7a5aa8f19269b1b21618036be0ff9",
+        "scores": [2, 1, 1, 4, 5, 5, 2, 3, 4, 2]
+    },
+    {
+        "name": "Jane",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuJd7HNnj2OFA5-MLfYXdqLlPpr5oZ2pkqjCGcSgpby1NQ2BdU",
+        "scores": [5, 3, 2, 1, 4, 5, 3, 1, 4, 2]
+    },
+    {
+        "name": "Ralph",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIvVWgZ0CI6jlnnDOvcrOi8GmG9E5kHRwEDoyjlHfpRNarv4SW",
+        "scores": [4, 2, 1, 4, 3, 1, 4, 3, 5, 1]
+    },
+    {
+        "name": "Mike",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBog2OcpxClAVatG8y-YTYA_kwo36DA7Ee_hJ8TeYQ6DefqBh-",
+        "scores": [3, 4, 1, 5, 2, 3, 4, 3, 1, 2]
+    },
+    {
+        "name": "Lillie",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8bpYRZkh35xMbCZklwgeyfcNnmX3xJnTYBG8qiSK9JUIl2X3K",
+        "scores": [5, 3, 2, 1, 4, 4, 2, 1, 4, 3]
+    },
+    {
+        "name": "Rebecca",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3VMFMS6iCSkWsB5Yv79jww8Ne1mzMavZT-0FTd5JaPmeTx2lnBA",
+        "scores": [1, 2, 4, 3, 4, 5, 1, 2, 3, 4]
+    },
+    {
+        "name": "Patricia",
+        "photo": " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfE3PHJEnihTVLRn_sEvz6Sxr0mpo1D_e6xZPI_FzqD4qj8280eg",
+        "scores": [4, 2, 1, 3, 2, 1, 3, 3, 5, 1]
+    },
+]
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+ 
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+
+// Static files
+// needs to be called before the routes in order to work
+app.use(express.static('app/public'));
+
+//Router
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
